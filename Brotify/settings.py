d772a57ba05ad844f91214a3927fdf9b13ec1dba
Basile Paradis <basileparadis@HAL-9000.local>
2020-03-22 19:14:33 -0400

    Ajout de la fonctionnalité de connexion et de déconnexion

--- Brotify/settings.py
+++ Brotify/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Brotify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Specify where to redirect the user upon a successful login/logout
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+

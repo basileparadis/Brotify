8e2c81db1854bb2d87ccab8cdd6a089888eaedeb
Basile Paradis <basileparadis@HAL-9000.local>
2020-03-28 20:59:49 -0400

    Modification mineures de l'interface

--- Brotify/settings.py
+++ Brotify/settings.py
@@ -136,3 +136,16 @@ STATICFILES_DIRS = [os.path.join(ROOT_PATH, 'static')]
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'biblio.views.save_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
--- biblio/models.py
+++ biblio/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+import requests
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    # user = User.objects.get(...)
+    #user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    #gender = models.CharField(max_length=20)
+    savedTracks = {}
+    savedAlbums = {}
+    followedArtists = {}
+    #social = user.social_auth.get(provider='spotify-oauth2')
+    #response = requests.get(
+    #    'https://api.spotify.com/v1/me/tracks',
+    #    params={'access_token': social.extra_data['access_token']}
+    #)
+    #friends = response.json()['items']
+    '''
+
+    print("Les chansons " + friends)
+    '''
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()

ee8eed1b655b0b8af793a4ee1fb6a215919bfabc
Basile Paradis <basileparadis@HAL-9000.local>
2020-05-14 19:33:14 -0400

    Affichage du statut d'amitié de│le.css.map utilisateurs

--- friends/views.py
+++ friends/views.py
@@ -1,27 +1,67 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.template import loader
-from friendship.models import Friend, Follow, Block
-
-# Create your views here.
+from friendship.models import Friend, FriendshipRequest
 from accounts.models import SocialUser
 
 
 @login_required
 def index(request):
-    liste_amis = []
+    liste_amis_id = []
+    list_sent_invitation_id = []
+    list_received_invitation_id = []
+
     users = User.objects.all()
     social_user = SocialUser.objects.all().order_by('-id')
-    friends = Friend.objects.all().filter(id=request.user.id)
+
+    # On vérifie les liens d'amitié entre un utilisateur
+    friends = Friend.objects.all()
     for friend in friends:
-        liste_amis.append(friend.to_user_id)
-    print(Friend.objects.all().filter(id__exact=request.user.id))
+        if request.user.id is friend.to_user_id:
+            liste_amis_id.append(friend.from_user_id)
+        elif request.user.id is friend.from_user_id:
+            liste_amis_id.append(friend.to_user_id)
+
+    # On vérifie les invitations envoyées d'un utilisateur
+    invitations = FriendshipRequest.objects.all()
+    for invitation in invitations:
+        if request.user.id is invitation.from_user_id:
+            list_sent_invitation_id.append(invitation.to_user_id)
+
+    # On vérifie les invitations reçus d'un utilisateur
+    invitations = FriendshipRequest.objects.all()
+    for invitation in invitations:
+        if request.user.id is invitation.to_user_id:
+            list_received_invitation_id.append(invitation.from_user_id)
+
     template = loader.get_template('friends.html')
     context = {
         'users': users,
         'social': social_user,
-        'amis': liste_amis,
+        'id_amis': liste_amis_id,
+        'id_invitation_sent': list_sent_invitation_id,
+        'id_invitation_received': list_received_invitation_id,
     }
     return HttpResponse(template.render(context, request))
+
+
+@login_required
+def accepter_amitie(request, otherUser):
+    try:
+        friend_request = FriendshipRequest.objects.get(from_user=request.user.id, to_user=otherUser.id)
+        friend_request.reject()
+    except Friend.DoesNotExist as exception:
+        print("La demande d'amitié est introuvable")
+        print(exception)
+
+
+@login_required
+def refuser_amitie(request, otherUser):
+    try:
+        friend_request = FriendshipRequest.objects.get(from_user=request.user.id, to_user=otherUser.id)
+        friend_request.accept()
+    except Friend.DoesNotExist as exception:
+        print("La demande d'amitié est introuvable")
+        print(exception)

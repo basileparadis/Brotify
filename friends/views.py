56507290d2f4dc7d7e4c04a04ace180d09c34b14
Basile Paradis <basileparadis@HAL-9000.local>
2020-05-15 12:53:37 -0400

    Les utilisateurs peuvent accepter des requêtes d'amitié

--- friends/views.py
+++ friends/views.py
@@ -9,59 +9,86 @@ from accounts.models import SocialUser
 
 @login_required
 def index(request):
-    liste_amis_id = []
-    list_sent_invitation_id = []
-    list_received_invitation_id = []
-
     users = User.objects.all()
     social_user = SocialUser.objects.all().order_by('-id')
+    template = loader.get_template('friends.html')
 
-    # On vérifie les liens d'amitié entre un utilisateur
+    # print(request.POST.get('accept_friend'))
+    if 'accept_friend' in request.POST:
+        accepter_amitie(request)
+    elif 'deny-friend' in request.POST:
+        refuser_amitie(request)
+    elif 'add-friend' in request.POST:
+        # accepter_amitie(request)
+        pass
+
+    context = {
+        'users': users,
+        'social': social_user,
+        'current_user': request.user,
+        'id_amis': get_friends(request),
+        'id_invitation_sent': get_sent_friend_request(request),
+        'id_invitation_received': get_received_friend_request(request),
+    }
+    return HttpResponse(template.render(context, request))
+
+
+# On vérifie les liens d'amitié entre un utilisateur
+@login_required
+def get_friends(request):
+    liste_amis_id = []
     friends = Friend.objects.all()
     for friend in friends:
         if request.user.id is friend.to_user_id:
             liste_amis_id.append(friend.from_user_id)
         elif request.user.id is friend.from_user_id:
             liste_amis_id.append(friend.to_user_id)
+    return liste_amis_id
 
-    # On vérifie les invitations envoyées d'un utilisateur
+
+# On vérifie les invitations envoyées d'un utilisateur
+@login_required()
+def get_sent_friend_request(request):
+    list_sent_invitation_id = []
     invitations = FriendshipRequest.objects.all()
     for invitation in invitations:
         if request.user.id is invitation.from_user_id:
             list_sent_invitation_id.append(invitation.to_user_id)
+    return list_sent_invitation_id
 
-    # On vérifie les invitations reçus d'un utilisateur
+
+# On vérifie les invitations reçus d'un utilisateur
+@login_required()
+def get_received_friend_request(request):
+    list_received_invitation_id = []
     invitations = FriendshipRequest.objects.all()
     for invitation in invitations:
         if request.user.id is invitation.to_user_id:
             list_received_invitation_id.append(invitation.from_user_id)
-
-    template = loader.get_template('friends.html')
-    context = {
-        'users': users,
-        'social': social_user,
-        'id_amis': liste_amis_id,
-        'id_invitation_sent': list_sent_invitation_id,
-        'id_invitation_received': list_received_invitation_id,
-    }
-    return HttpResponse(template.render(context, request))
+    return list_received_invitation_id
 
 
 @login_required
-def accepter_amitie(request, otherUser):
+def accepter_amitie(request):
+    ami_potentiel_id = int(request.POST.get('accept_friend'))
+    print(ami_potentiel_id)
+    print(request.user.id)
     try:
-        friend_request = FriendshipRequest.objects.get(from_user=request.user.id, to_user=otherUser.id)
-        friend_request.reject()
-    except Friend.DoesNotExist as exception:
+        friend_request = FriendshipRequest.objects.get(from_user=ami_potentiel_id, to_user=request.user.id)
+        # friend_request = FriendshipRequest.objects.all()[0].__dict__
+        # print(friend_request)
+        friend_request.accept()
+    except FriendshipRequest.DoesNotExist as exception:
         print("La demande d'amitié est introuvable")
         print(exception)
 
 
 @login_required
-def refuser_amitie(request, otherUser):
+def refuser_amitie(request):
+    ami_potentiel_id = request.POST.get('deny-friend')
     try:
-        friend_request = FriendshipRequest.objects.get(from_user=request.user.id, to_user=otherUser.id)
-        friend_request.accept()
-    except Friend.DoesNotExist as exception:
+        friend_request = FriendshipRequest.objects.get(from_user=request.user.id, to_user=ami_potentiel_id)
+        friend_request.reject()
+    except FriendshipRequest.DoesNotExist as exception:
         print("La demande d'amitié est introuvable")
         print(exception)

ce864ac5d87c53285968d8fd38ec0c5737e4d281
Basile Paradis <basile.paradis@gmail.com>
2020-05-21 10:27:14 -0400

    Amélioration de la vue (menu hamburger & nouvelle police)

--- biblio/models.py
+++ biblio/models.py
@@ -15,44 +15,59 @@ class Singleton(type):
 
 
 class Track(models.Model):
-    id_track = models.CharField(max_length=24, default='')
-    title = models.CharField(max_length=100, default='')
-    artist = models.CharField(max_length=100, default='')
-    album = models.CharField(max_length=100, default='')
-    url_cover = models.CharField(max_length=100, default='')
-    url_track = models.CharField(max_length=100, default='')
+    id_track = models.CharField(max_length=24)
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    url_cover = models.CharField(max_length=100)
+    url_track = models.CharField(max_length=100)
+    date_added = models.DateTimeField()
     # artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     # album = models.ForeignKey(Album, on_delete=models.CASCADE)
 
 
-class Biblio(metaclass=Singleton):
-    user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
-    user_songs = models.ForeignKey(Track, related_name='user_songs', on_delete=models.CASCADE)
+class Biblio(models.Model):
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    user_song = models.ForeignKey(Track, related_name='user_song', on_delete=models.CASCADE, null=True)
     # user_songs = models.ForeignKey('app_label.Track', on_delete=models.CASCADE)
-    liste_chansons = []
+    # liste_chansons = []
     # savedAlbums = {}
     # followedArtists = {}
 
     def vider_biblio(self):
         try:
             # Biblio.__call__().objects.all().delete()
-            del self.liste_chansons[:]
+            # del self.liste_chansons[:]
+            Biblio.objects.all().delete()
+            Track.objects.all().delete()
         except Exception as exception:
             print(exception)
 
-    def get_chansons(self):
-        return self.liste_chansons
 
-    def ajouter_chansons(self, data):
-        for i in range(0, len(data['items'])):
-            les_chansons = data['items'][i]['track']
-            # Si la chanson n'a pas de pochette, on en attribue une générique
-            try:
-                cover = les_chansons['album']['images'][1]['url']
-            except IndexError:
-                cover = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Circle-icons-music.svg/' \
-                        '512px-Circle-icons-music.svg.png'
-            # La chanson
+def get_chansons(user):
+    # return self.liste_chansons
+    # return Biblio.objects.filter(user=user).user_song_id
+    # return Track.objects.filter(id__in=Biblio.objects.filter(user=user).values_list('user_song_id', flat=True))
+    les_chansons = []
+    tracks = Track.objects.filter(id__in=Biblio.objects.filter(user=user).values_list('user_song_id', flat=True))
+    for track in tracks:
+        les_chansons.append(track)
+    return les_chansons
+
+
+def ajouter_chansons(data, user):
+    for i in range(0, len(data['items'])):
+        les_chansons = data['items'][i]['track']
+        # Si la chanson n'a pas de pochette, on en attribue une générique
+        try:
+            cover = les_chansons['album']['images'][1]['url']
+        except IndexError:
+            cover = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Circle-icons-music.svg/' \
+                    '512px-Circle-icons-music.svg.png'
+        # On ajoute la chanson si elle n'existe pas déjà dans la BD
+        try:
+            track = Track.objects.get(id_track=les_chansons['id'])
+        except Track.DoesNotExist:
             track = Track.objects.create(
                 id_track=les_chansons['id'],
                 title=les_chansons['name'],
@@ -60,9 +75,16 @@ class Biblio(metaclass=Singleton):
                 album=les_chansons['album']['name'],
                 url_cover=cover,
                 url_track=les_chansons['external_urls']['spotify'],
+                date_added=data['items'][i]['added_at'],
             )
+        try:
+            Biblio.objects.create(user=user, user_song=track)
+        except Exception as exception:
+            print(exception)
 
-            Biblio.__call__().get_chansons().append(track)
+            # self.get_chansons().append(track)
+            # Biblio.objects.add(track)
+            # Biblio.__call__().get_chansons().append(track)
 
 
 class Artist(models.Model):

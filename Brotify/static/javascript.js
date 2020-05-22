c7645c42abf7578208b7d3f8e4b5c4b263a1d081
Basile Paradis <basile.paradis@gmail.com>
2020-05-22 13:55:57 -0400

    Disposition de la biblio de l'utilisateur en mosaïque et affichage en tooltip d'un court extrait audio pour chacun

--- Brotify/static/javascript.js
+++ Brotify/static/javascript.js
@@ -1,7 +1,36 @@
 $(document).ready(() => {
 
+    $('.tooltip').each(function() { // Grab all elements with a title attribute,and set "this"
+        if(/None/g.test($(this).attr('data-*'))) {
+            var text = 'Aperçu indisponible'
+        }
+        else {
+            var text = "<video width='245' height='190' controls><source src='"+$(this).attr('data-*')+"' type='audio/mpeg'></video>"
+        }
+        $(this).qtip({
+            style: {
+                color: 'black'
+            },
+            content: {
+                title: $(this).attr('title')+' par '+$(this).attr('alt'),
+                text: text
+            },
+            position: {
+                target: 'mouse', // Track the mouse as the positioning target
+                adjust: {
+                    mouse: false // Don't adjust continuously the mouse, just use initial position
+                }
+            },
+            show: {
+                solo: true
+            },
+            hide: false
+        });
+    });
+
     let $hamburger = $(".hamburger");
     $hamburger.on("click", () => {
+        $hamburger.toggleClass("is-active");
         $( "#navigation" ).animate({
             width: "toggle",
             height: "toggle"

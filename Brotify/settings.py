7c4f2290cb96bfdab4b25be7d003ad555a0ba73f
basile <zocfAf-4qypbi-bakpyq>
2021-03-25 22:00:37 -0400

    Docker déploie sur le port 80

--- Brotify/settings.py
+++ Brotify/settings.py
@@ -28,9 +28,9 @@ SOCIAL_AUTH_SPOTIFY_SCOPE = ['user-read-email', 'user-library-read']
 SECRET_KEY = '1rv==iz)yoao&f+q6!8@b6_q8h8poxhqusz@-5pig(ka4x&f(1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.brotify.ca', '127.0.1.1']
 USE_X_FORWARDED_PORT = True
 
 # Application definition

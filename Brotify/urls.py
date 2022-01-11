885d20fb03c56c38c79631bea614825812ced2ad
basileparadis <basile.paradis@gmail.com>
2022-01-11 13:58:27 -0500

    Remove old static directory and added robots.txt

--- Brotify/urls.py
+++ Brotify/urls.py
@@ -29,9 +29,10 @@ urlpatterns = [
     path('', include('friends.urls')),
     path('', include('accounts.urls')),
     path('', TemplateView.as_view(template_name='biblio.html'), name='home'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
     re_path(r'^celery-progress/', include('celery_progress.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
